e86c634cb3f6c448ce7c881af1ca2ec6e1a77a01
dvinokurov <dvinokurov777@gmail.com>
2014-08-06 20:07:51 +0300

    Lifted inserts work for arguments of type (Int,String)

--- src/main/scala/scala/slick/mongodb/lifted/MongoInsertInvokerComponent.scala
+++ src/main/scala/scala/slick/mongodb/lifted/MongoInsertInvokerComponent.scala
@@ -4,10 +4,10 @@ package scala.slick.mongodb.lifted
 import com.mongodb.casbah.Imports._
 import com.mongodb.WriteResult
 import com.mongodb.casbah.BulkWriteResult
-import com.mongodb.casbah.commons.MongoDBObject
+//import com.mongodb.casbah.commons.MongoDBObject
 
 import scala.annotation.implicitNotFound
-import scala.slick.ast.{Node, TableExpansion, TableNode}
+import scala.slick.ast._
 import scala.slick.mongodb.direct.{GetResult, TypedMongoCollection}
 import scala.slick.profile.BasicInsertInvokerComponent
 
@@ -15,19 +15,31 @@ import scala.language.implicitConversions
 
 trait MongoInsertInvokerComponent extends BasicInsertInvokerComponent{ driver: MongoDriver =>
   // TODO: generify: InsertInvoker -> InsertInvoker[T]
-  override type InsertInvoker = InsertInvokerDef
+  override type InsertInvoker[T] = InsertInvokerDef[T]
 
   /** Create an InsertInvoker -- this method should be implemented by drivers as needed */
-  override def createInsertInvoker[T](compiled: CompiledInsert): InsertInvoker = new InsertInvokerDef(compiled)
+  override def createInsertInvoker[T](compiled: CompiledInsert): InsertInvoker[T] = new InsertInvokerDef(compiled)
   override type CompiledInsert = Node     // TODO: change to MongoNode when moving from SQL node tree to mongo tree
 
   // TODO: use mongo-specific nodes, add support for nested document structure
   @implicitNotFound("Implicit converter of type ${T}=>DBObject required for MongoDB InsertInvoker")
-  class InsertInvokerDef(val node: CompiledInsert) extends super.InsertInvokerDef[(Int,String)] {
+  class InsertInvokerDef[T](val node: CompiledInsert) extends super.InsertInvokerDef[T] {
     /** Used to convert data from DBObject to specified type after find operation - required for TypedMongoCollection creation */
-    val converter: GetResult[(Int,String)] = ???
+    val converter: GetResult[(Int,String)] =
+      GetResult[(Int,String)](r => (r.get(attributeNames._1).asInstanceOf[Int],r.get(attributeNames._2).asInstanceOf[String]))
     /** Used to convert specified type to DBObject */
-    val binder: (Int,String) => MongoDBObject = ???
+    val binder: ((Int,String)) => MongoDBObject = { t: (Int,String) =>
+      val a = attributeNames
+      MongoDBObject(List((a._1,t._1),(a._2,t._2)))
+    }
+
+    /** Used to retrieve attribute names from the query (CompiledInsert) */
+    private def attributeNames: (String,String) = node match {
+      case TableExpansion(_,_,pn: ProductNode) =>
+        val nodes = pn.nodeChildren.take(2).toList
+        println(s"types: ${nodes(0).getClass},${nodes(1).getClass}")
+        (nodes(0).asInstanceOf[Select].field.name,nodes(1).asInstanceOf[Select].field.name)
+    }
 
     /** Collection requires session to be instantiated, so we have to use var+def instead of lazy val here */
     private def collection(session: Backend#Session) = cachedCollection match{
@@ -50,11 +62,12 @@ trait MongoInsertInvokerComponent extends BasicInsertInvokerComponent{ driver: M
     override type MultiInsertResult = BulkWriteResult
 
     /** Insert a single value */
-    override def +=(value: (Int,String))(implicit session: Backend#Session): SingleInsertResult = collection(session).insert(binder(value._1,value._2))
+    override def +=(value: T)(implicit session: Backend#Session): SingleInsertResult =
+      collection(session).insert(binder(value.asInstanceOf[(Int,String)]))
     /** Insert a collection of values */
-    override def ++=(values: Iterable[(Int,String)])(implicit session: Backend#Session): MultiInsertResult = {
+    override def ++=(values: Iterable[T])(implicit session: Backend#Session): MultiInsertResult = {
       val builder = collection(session).initializeOrderedBulkOperation
-      for {document <- values} builder.insert(binder(document._1,document._2))
+      for {document <- values} builder.insert(binder(document.asInstanceOf[(Int,String)]))
       builder.execute()
     }
   }
